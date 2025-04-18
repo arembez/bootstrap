11537fca9f2e45024059d24e1d4dfa18e78f00ce
Alex Rembez <arembez@gmail.com>
2025-04-18 14:57:31 +0300

    Added security key for gitlab CI/CD

--- security.tf
+++ security.tf
@@ -29,25 +29,22 @@ resource "yandex_kms_symmetric_key" "key" {
   rotation_period = "8760h"
 }
 
-# Создание сервисного акканута для Terraform
-#resource "yandex_iam_service_account" "sa-gitlab-tf" {
-#  name        = "gitlab-terraform-sa"
-#  description = "Service account for Terraform in Gitlab"
-#}
+# Service account for gitlab CI/CD
+resource "yandex_iam_service_account" "sa-gitlab-tf" {
+  name        = "gitlab-terraform-sa"
+  description = "Service account for Terraform in Gitlab"
+}
 
-# Назначение роли сервисному аккаунту - admin
-#resource "yandex_resourcemanager_folder_iam_member" "sa_terraform_admin" {
-#  folder_id = data.yandex_client_config.client.folder_id
-#  role      = "admin"
-#  member    = "serviceAccount:${yandex_iam_service_account.sa-gitlab-tf.id}"
-#}
+# !!! Admin role for service account !!!
+resource "yandex_resourcemanager_folder_iam_member" "sa_terraform_admin" {
+  folder_id = data.yandex_client_config.client.folder_id
+  role      = "admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-gitlab-tf.id}"
+}
 
-# Создание авторизованного ключа доступа
-#resource "yandex_iam_service_account_key" "sa-auth-key" {
-#  service_account_id = "${yandex_iam_service_account.sa-gitlab-tf.id}"
-#  description        = "Key for service account"
-#  key_algorithm      = "RSA_2048"
-#}
-
-# Создание файла .key.json с ключом доступа для Terraform
-# "service_account_id": "${yandex_iam_service_account.sa-gitlab-tf.id}",
+# Auth key for service account
+resource "yandex_iam_service_account_key" "sa-auth-key" {
+  service_account_id = "${yandex_iam_service_account.sa-gitlab-tf.id}"
+  description        = "Key for service account"
+  key_algorithm      = "RSA_2048"
+}
