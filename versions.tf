23d33a2923a26d739093056505603d45d914762e
Alex Rembez <arembez@gmail.com>
2025-04-18 13:32:31 +0300

    Providers replacement

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 terraform {
+   required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~>0.130"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
